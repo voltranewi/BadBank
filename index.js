10f5ff37bfea10217b7101ea62a17d507220ad36
Victor Oltra <93753070+voltranewi@users.noreply.github.com>
2022-10-27 18:38:53 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-var express = require('express');
-var app = express();
-var cors = require('cors');
-var dal = require('./dal.js');
+function Spa() {
+  return (
+    <HashRouter>
+      <div>
+        <NavBar/>        
+        <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+          <div className="container" style={{padding: "20px"}}>
+            <Route path="/" exact component={Home} />
+            <Route path="/CreateAccount/" component={CreateAccount} />
+            <Route path="/login/" component={Login} />
+            <Route path="/deposit/" component={Deposit} />
+            <Route path="/withdraw/" component={Withdraw} />
+            {/* <Route path="/transactions/" component={Transactions} /> */}
+            <Route path="/balance/" component={Balance} />
+            <Route path="/alldata/" component={AllData} />
+          </div>
+        </UserContext.Provider>
+      </div>
+    </HashRouter>
+  );
+}
 
-app.use(express.static('public'));
-app.use(cors());
-
-app.get('/account/create/:name/:email/:password', function (req, res){
-    dal.create(req.params.name, req.params.email, req.params.password).
-    then((user) => {
-        console.log(user);
-        res.send(user);
-    });
-});
-
-app.get('/account/login/:email/password', function (req, res){
-    res.send({
-        email: req.params.email,
-        password: req.params.password
-    });
-});
-
-app.get('/account/all', function (req, res){
-    dal.all().
-    then((docs) => {
-        console.log(docs);
-        res.send(docs);
-    });
-});
-
-var port= 3000;
-app.listen(port);
-console.log('Running on port: ' + port);
+ReactDOM.render(
+  <Spa/>,
+  document.getElementById('root')
+);
